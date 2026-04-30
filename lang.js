ee5f2a2276221e0a42c38d77b646cc9ceb70d341
konradxu <konrad.xu08@gmail.com>
2026-04-30 12:11:21 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -9,7 +9,7 @@
    Change them once, they update in all three languages.
    ════════════════════════════════════════════════════════════ */
 const SITE_INFO = {
-  name:       'Konrad Xu, Marlene Reitmaier (Co-Founder)',
+  name:       'Konrad Xu, Marlene Reitmaier (Co-Founder), Felicitas Bauer (Unterstützung)',
   street:     '',           // street + number
   postalCity: '',             // postal code + city
   email:      'kontakt@cityarchivist.com',
